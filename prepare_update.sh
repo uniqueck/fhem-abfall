09124354463c1be8bdff2db4f3d059057a3fe52f
Constantin Krüger <Constantinidis@web.de>
2016-02-11 01:52:14 +0100

    change format for date and time

--- prepare_update.sh
+++ prepare_update.sh
@@ -2,7 +2,7 @@
 rm controls_fhemabfall.txt
 find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
   do
-   out="UPD "$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f)" "$(stat -f%z $f)" ${f}"
+   out="UPD "$(stat -c %y  $f | cut -d. -f1)" "$(stat -c %s $f)" ${f}"
    echo ${out//.\//} >> controls_fhemabfall.txt
 done
 
