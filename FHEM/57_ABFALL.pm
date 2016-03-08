b3246eda3ef4ea7fce27928c205813095d4f2bc1
Constantin Krüger <Constantinidis@web.de>
2016-03-08 08:50:19 +0100

    fix an error that crashs fhem

--- FHEM/57_ABFALL.pm
+++ FHEM/57_ABFALL.pm
@@ -124,7 +124,7 @@ sub ABFALL_GetUpdate($){
 			if($nowAbfall_text eq "") {
 				$nowAbfall_text = $termin->{summary};	
 			} else {
-				$nowAbfall_text .= $delimiter_text_reading ._ $termin->{summary};
+				$nowAbfall_text .= $delimiter_text_reading . "_" . $termin->{summary};
 			}
 			$nowAbfall_tage = $termin->{tage};
 			$nowAbfall_datum = $termin->{bdate};
