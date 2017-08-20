a1c6b151e7b51f9f01a54898e737f0f29a19b16f
Constantin Krüger <Constantinidis@web.de>
2017-08-20 14:31:55 +0200

    fix formatted date issue

--- FHEM/57_ABFALL.pm
+++ FHEM/57_ABFALL.pm
@@ -214,7 +214,7 @@ sub ABFALL_GetUpdate($){
 				$nowAbfall_description .= $delimiter_text_reading . $event->{description};
 			}
 			$nowAbfall_tage = $event->{days};
-			$nowAbfall_datum = $event->{date};
+			$nowAbfall_datum = $event->{dateFormatted};
 			$nowAbfall_weekday = $event->{weekday};
 			$now_readingTermin = $readingTermin;
 		} elsif	($nextAbfall_tage == -1 || $nextAbfall_tage > $event->{days} || $nextAbfall_tage == $event->{days} ) {
@@ -238,7 +238,7 @@ sub ABFALL_GetUpdate($){
 				$nextAbfall_description .= $delimiter_text_reading . $event->{description};
 			}
 			$nextAbfall_tage = $event->{days};
-			$nextAbfall_datum = $event->{date};
+			$nextAbfall_datum = $event->{dateFormatted};
 			$nextAbfall_weekday = $event->{weekday};
 			if ($next_readingTermin eq "") {
 				$next_readingTermin = $readingTermin;
@@ -259,8 +259,8 @@ sub ABFALL_GetUpdate($){
 		readingsBulkUpdate($hash, $readingTermin ."_tage", $event->{days}) if ($enable_old_readingnames);
 		readingsBulkUpdate($hash, $readingTermin ."_days", $event->{days});
 		readingsBulkUpdate($hash, $readingTermin ."_text", $event->{summary});
-		readingsBulkUpdate($hash, $readingTermin ."_datum", $event->{date}) if ($enable_old_readingnames);
-		readingsBulkUpdate($hash, $readingTermin ."_date", $event->{date});
+		readingsBulkUpdate($hash, $readingTermin ."_datum", $event->{dateFormatted}) if ($enable_old_readingnames);
+		readingsBulkUpdate($hash, $readingTermin ."_date", $event->{dateFormatted});
 		readingsBulkUpdate($hash, $readingTermin ."_wochentag", $event->{weekday}) if ($enable_old_readingnames);
 		readingsBulkUpdate($hash, $readingTermin ."_weekday", $event->{weekday});
 		readingsBulkUpdate($hash, $readingTermin ."_location", $event->{location});
@@ -471,7 +471,8 @@ sub getEvents($){
 						$foundItem->{weekday} = $wdayname;
 						$foundItem->{location} = $eventLocation;
 						$foundItem->{description} = $eventDescription;
-						$foundItem->{date} = $eventDateFormatted;
+						$foundItem->{date} = $eventDate;
+						$foundItem->{dateFormatted} = $eventDateFormatted;
 						$foundItem->{days} = $dayDiff;
 					}
 				} else {
@@ -485,9 +486,10 @@ sub getEvents($){
 						location => $eventLocation,
 						description => $eventDescription,
 						readingName => $cleanReadingName,
-						date => $eventDateFormatted,
+						date => $eventDate,
 						days => $dayDiff,
-						calendar => $calendername};
+						calendar => $calendername,
+						dateFormatted => $eventDateFormatted};
 				}
 			}
 		} # end for each uid
--- README.md
+++ README.md
@@ -5,20 +5,20 @@ ABFALL is a fhem modul, which creates readings based on one or more calendar dev
 The Perl module can be loaded directly into your FHEM installation. For this please copy the below command into the FHEM command line.
 
 	update all https://raw.githubusercontent.com/uniqueck/fhem-abfall/master/controls_fhemabfall.txt
-	
+
 ### Create a device
-	
+
 	define myABFALL ABFALL <name of a calendar device>
-	
+
 ### Attributes
-	
+
 - abfall\_clear\_reading_regex
 
-	regex to delete a part of the summary of an event 
-- disable	
-	
+	regex to delete a part of the summary of an event
+- disable
+
 	valid values 0 and 1, set to 1 to disable device
-	
+
 - weekday_mapping
 
 	set a map of names for the weekdays, for example Su Mo Tue Wed Thu Fr Sa
@@ -29,20 +29,26 @@ The Perl module can be loaded directly into your FHEM installation. For this ple
 	only useful for multiple calendars, add calendar name as preafix on readingname, if you define a new abfall device with only one calendar device,
 	it is set to 0
 - delimiter\_text_reading
-	
+
 	if more than one event exist on the same day, this is the delimiter to join these events
-	only for the readings next_text, next_location, next_description, now_text, now_location and now_description 
+	only for the readings next_text, next_location, next_description, now_text, now_location and now_description
 - delimiter_reading
-	
+
 	same as delimiter_text_reading, but only for the reading next and now
 - filter
 	include only events, which match these filter condition, the filter condition can be a normale text or regex expression
 - enable_counting_pickups
-	
+
 	activate support for counting pickups
 - enable_old_readingnames
 
 	add old deprecated german readings, in the future, all readings are english
+
+- date_style
+
+	valid values are date and dateTime, date is the default value
+	control the style of all date reading
+
 
 ## How to Update
 The Perl module can be update directly with standard fhem update process. For this please copy the below command into the FHEM command line.
